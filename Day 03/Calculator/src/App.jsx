84f882349d6c27e26c5bbc3a69bae0e859fdfbf9
Syed Muhammad Zafar Rizvi <69090919+ZafarRizvi@users.noreply.github.com>
2023-12-02 01:59:39 +0500

    Calculator UI

--- Day 03/Calculator/src/App.jsx
+++ Day 03/Calculator/src/App.jsx
@@ -1,35 +1,131 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fValue, setFValue] = useState("0");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center h-screen items-center bg-black">
+        <div className="bg-slate-400 w-fit h-fit px-5 py-3 rounded-lg">
+          <div className="flex justify-center">
+            <h1 className="font-bold text-lg pb-3">Calculator</h1>
+          </div>
+          <form className="flex flex-col">
+            <input
+              type="text"
+              className="bg-slate-200 px-3 py-2 rounded-lg mb-3"
+              placeholder="0"
+              value={fValue}
+              readOnly
+            />
+            <div className="grid grid-cols-4 gap-3">
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("1")}
+              >
+                1
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("2")}
+              >
+                2
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("3")}
+              >
+                3
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("+")}
+              >
+                +
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("4")}
+              >
+                4
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("5")}
+              >
+                5
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("6")}
+              >
+                6
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+              >
+                -
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("7")}
+              >
+                7
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("8")}
+              >
+                8
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("9")}
+              >
+                9
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+              >
+                *
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+                onClick={() => setFValue("0")}
+              >
+                0
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg"
+              >
+                .
+              </button>
+              <button
+                type="button"
+                className="bg-slate-200 px-3 py-2 rounded-lg grid col-span-2"
+              >
+                =
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Day 03/Calculator/src/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ btnText }) => {
+  return (
+    <div>
+      <h1>{btnText}</h1>
+    </div>
+  );
+};
+
+export default Button;
