0886fd18a8dada96fbb0df33ac3d47abc9599455
Syed Muhammad Zafar Rizvi <69090919+ZafarRizvi@users.noreply.github.com>
2024-01-30 17:23:49 +0500

    Tic Tac Toe

--- Day 12/tictactoe/src/components/Tic Tac Toe/TicTacToe.jsx
+++ Day 12/tictactoe/src/components/Tic Tac Toe/TicTacToe.jsx
@@ -1,35 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./tictactoe.css";
-import Cicle from "./../asserts/circle.png";
-import Cross from "./../asserts/cross-red.png";
 
 const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(""));
+
   return (
     <div className="main-div">
       <h1>
-        Tac Tac Toe in <span>ReactJS</span>
+        Tic Tac Toe using <span>ReactJS</span>
       </h1>
       <div className="game-div">
-        <div className="row">
-          <div className="box" id="box-1">
-            <img src={Cicle} alt="circle" className="circle" />
-          </div>
-          <div className="box" id="box-2">
-            <img src={Cross} alt="circle" className="circle" />
-          </div>
-          <div className="box" id="box-3"></div>
-        </div>
-        <div className="row">
-          <div className="box" id="box-4"></div>
-          <div className="box" id="box-5"></div>
-          <div className="box" id="box-6"></div>
-        </div>
-        <div className="row">
-          <div className="box" id="box-7"></div>
-          <div className="box" id="box-8"></div>
-          <div className="box" id="box-9"></div>
-        </div>
+        {board.map((value, index) => {
+          return (
+            <div className="box" key={index}>
+              {value}
+            </div>
+          );
+        })}
       </div>
+      <button className="reset-btn">RESET</button>
     </div>
   );
 };
