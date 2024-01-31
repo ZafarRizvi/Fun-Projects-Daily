0394322503a04b4baa51c0e9ce2c36b89a4216f4
Syed Muhammad Zafar Rizvi <69090919+ZafarRizvi@users.noreply.github.com>
2024-01-31 23:33:50 +0500

    31-01

--- Day 12/tictactoe/src/components/Tic Tac Toe/TicTacToe.jsx
+++ Day 12/tictactoe/src/components/Tic Tac Toe/TicTacToe.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import "./tictactoe.css";
+import crossIcon from "./../asserts/cross.png";
+import circleIcon from "./../asserts/circle.png";
 
 const TicTacToe = () => {
   const [board, setBoard] = useState(Array(9).fill(""));
+  const [turn, setTurn] = useState("O");
+  const [disabledBoxes, setDisabledBoxes] = useState(Array(9).fill(false));
+
+  const handleClick = (index) => {
+    if (board[index] === "" && !disabledBoxes[index]) {
+      setTurn(turn === "X" ? "O" : "X");
+    }
+  };
 
   return (
     <div className="main-div">
@@ -12,12 +22,24 @@ const TicTacToe = () => {
       <div className="game-div">
         {board.map((value, index) => {
           return (
-            <div className="box" key={index}>
+            <div
+              className="box"
+              key={index}
+              onClick={() => handleClick(index)}
+              disabled={disabledBoxes[index]}
+            >
               {value}
             </div>
           );
         })}
       </div>
+      <h1 className="won-msg">
+        Turn ={" "}
+        <img
+          src={turn === "X" ? crossIcon : circleIcon}
+          alt={turn === "X" ? "Cross icon" : "Circle icon"}
+        />
+      </h1>
       <button className="reset-btn">RESET</button>
     </div>
   );
